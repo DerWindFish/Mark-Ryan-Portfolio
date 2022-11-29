76702b4bb65fa961b592d3467334e5bbb67ebf5f
Mark Ryan <markrryan99@gmail.com>
2022-11-29 12:40:41 -0800

    added to about me section

---
+++ mark-ryan-portfolio/src/components/About.jsx
@@ -0,0 +1,24 @@
+const About = () => {
+    return(
+    <div>
+        <h3 className="aboutme">I'm a software developer from Orange, CA.</h3>
+        <p className="aboutme">I love coffee, I am an avid D&D player, I enjoy always learning new things.</p>
+        <p className="aboutme">
+            In the fall of 2021, being tired and burnt out at my job as a 
+        supervisor at Starbucks, I sat down with a friend of mine who had just recently taken 
+        a coding bootcamp and landed a job and talked about how he did it.
+        That converstation set me down the path I am on today.</p>
+
+        <p>
+        <b>Some of my skills include:</b>
+        </p>
+        <li><b>Languages</b> - JavaScript, HTML, CSS, Python, SQL, Mongo, Sequelize, JSON</li>
+        <li><b>Database</b> -  PostgreSQL,  MongoDB | Other - RESTful Routing, JSON api</li>
+        <li><b>Libraries and Frameworks</b> - React, Express.js, Django </li>
+        
+
+    </div>
+    )
+}
+
+export default About
