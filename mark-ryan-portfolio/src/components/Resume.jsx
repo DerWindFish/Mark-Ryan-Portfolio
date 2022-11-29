9a761928078a6616cbd43c77e61ad057d717e600
Mark Ryan <markrryan99@gmail.com>
2022-11-29 12:42:04 -0800

    added resume info to resume.jsx

---
+++ mark-ryan-portfolio/src/components/Resume.jsx
@@ -0,0 +1,19 @@
+const Resume = () => {
+
+    return (
+      <div>
+        <p><b>Email:</b> markrryan99@gmail.com</p>
+        <p><b>Phone:</b> 714-363-6730</p>
+        <p><b>Resume:  
+        <a
+          href="https://docs.google.com/document/d/14UYEoV34NeDJd1QYzxLOtcQJiLDpAFmgvI1JzzhJNZI/edit?usp=sharing"
+          download
+        >
+           Click Here To View
+        </a></b></p>
+      </div>
+    );
+  }
+  
+  
+  export default Resume
