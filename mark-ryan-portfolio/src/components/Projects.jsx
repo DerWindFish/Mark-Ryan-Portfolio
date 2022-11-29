b004e32eefb783f69cc84d0d83161df279157998
Mark Ryan <markrryan99@gmail.com>
2022-11-29 12:41:23 -0800

    added basics into projects.jsx

---
+++ mark-ryan-portfolio/src/components/Projects.jsx
@@ -0,0 +1,39 @@
+const Projects = () => {
+    return(
+      <div>
+  
+        <br />
+        <img src='https://i.imgur.com/PPNcKeR.png' alt='connect 4 game' className="connect" />
+        <p>My first project. A simple game of Connect-4 that you can play with a friend</p>
+        <li>- Used Vanilla Javascript, CSS3, HTML5, in order to make the game.</li>
+        <li>- Implemented game logic to determine all combinations of wins and draws.</li>
+        <br />
+        <a href="https://github.com/DerWindFish/Connect-4-A-Game-About-Connecting-4">GitHub Code</a>
+  
+        <br />
+        <br />
+  
+        <img src='https://i.imgur.com/hdjwDUj.png' alt='mushi roll' className="mushi-roll" />
+        <p>Full-stack application where users can create, edit, and delelte their own anime watchlists, as well as view the watchlists of other users</p>
+        <li>- Utilized a PERN stack (PostgreSQL, Express, React.js, Node.js) to build a full stack application with a team. With full CRUD hitting a RESTful API.</li>
+        <li>- Built a beautiful Full-Stack application in a week with the help of a team.</li>
+        <li>- Used Sequelize and PostgreSQL to connect data with the use of a through-table.</li>
+        <br />
+        <a href="https://github.com/DerWindFish/Mushi_Roll_Frontend">GitHub Code</a>
+  
+        <br />
+        <br />
+  
+        <img src='https://i.imgur.com/6XJp3yq.png' alt='WeRatePets' className="mushi-roll" />
+        <p>A whimsical full-stack application, where users can post pictures of their pets to be rated by themselves and others</p>
+        <li>- Utilized a PERN stack (PostgreSQL, Express, React.js, Node.js) to build a full stack application. With full create, read, update, delete (CRUD) operations hitting all RESTful API calls.</li>
+        <li>- Included full user authentication using JWT (Json web tokens)</li>
+        <br />
+        <a href="https://github.com/DerWindFish/RateMyPet">GitHub Code</a>
+  
+      </div>
+    )
+      
+  }
+  
+  export default Projects
